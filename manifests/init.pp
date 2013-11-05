bb35ab47a66e29bbd1293a46d226ce86a7c1536a
Atsushi Nagase <a@ngs.io>
2013-11-05 16:27:51 +0900

    4.1.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class garmin_communicator_plugin {
   package { 'Garmin Communicator Plugin':
-    source   => 'https://www8.garmin.com/software/CommunicatorPluginforMac_404.dmg',
+    source   => 'https://www8.garmin.com/software/CommunicatorPluginforMac_410.dmg',
     provider => 'pkgdmg'
   }
 }
